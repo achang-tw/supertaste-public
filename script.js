0433d31b44e305c42341873bb977757aeb0c7215
linsly-achang <76680239+linsly-achang@users.noreply.github.com>
2025-02-26 18:01:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
 const SuperCoupon = {
-	coupons: [
-		{
-			"title": "金田屋大福飲品買一送一",
-			"txt": "金田屋大福飲品買一送一，限定活動，快來搶購！",
-			"date": "02/24 開始",
-			"href": "https://supertaste.tvbs.com.tw/food/353033"
-		},
-		{
-			"title": "金田屋大福飲品，限定日期買一送一",
-			"txt": "金田屋大福飲品，買一送一，限定日期，不要錯過！",
-			"date": "02/24 開始",
-			"href": "https://supertaste.tvbs.com.tw/food/353033",
-			"img": "https://linsly-achang.github.io/supertaste-public/IMG_3888.jpg"
-		},
-		{
-			"cover": "https://linsly-achang.github.io/supertaste-public/%E5%84%AA%E6%83%A0%E5%88%B8_0212.png",
-			"title": "金田屋大福飲品，限定日期買一送一",
-			"href": "https://supertaste.tvbs.com.tw/food/353033"
-		}
-	],
+	coupons: [],
 	async init(){
 		await new Promise(resolve => setTimeout(resolve, 1000));
 
@@ -93,73 +74,78 @@ const SuperCoupon = {
 	},
 	async loadCoupons(){
 		fetch('https://ads.achang.tw/super-coupon/index.php').then(res => res.json()).then(data => {
-			const fetchData = JSON.parse(data);
-			console.log(fetchData)
-		});
-		const promises = Array.from(document.querySelectorAll('.supertaste-coupon .coupon-list')).map(container => {
-			return new Promise(resolve => {
-				container.innerHTML = this.coupons.map(coupon => {
-					if(coupon.cover) {
-						return `<div class="coupon-item splide__slide">
-							<div class="coupon-item-wrap">
-								<div><a href="${coupon.href}" target="_blank"><img src="${coupon.cover}" alt="${coupon.title}"></a></div>
-							</div>
-						</div>`
-					} else if(coupon.img) {
-						coupon = `<div class="coupon-item splide__slide">
-							<div class="coupon-item-wrap">
-								<div class="coupon-item-img" style="background-image:url(${coupon.img})"></div>
-								<div class="coupon-item-content">
-									<div><a href="${coupon.href}" target="_blank">${coupon.title}</a></div>
-									<div>${coupon.txt}</div>
-									<div>${coupon.date}</div>
-								</div>
-							</div>
-						</div>`;
-					}else{
-						coupon = `<div class="coupon-item splide__slide">
-							<div class="coupon-item-wrap no-img">
-								<div class="coupon-item-content">
-									<div><a href="${coupon.href}" target="_blank">${coupon.title}</a></div>
-									<div>${coupon.txt}</div>
-									<div>${coupon.date}</div>
-								</div>
-							</div>
-						</div>`;
-					}
+			this.coupons = JSON.parse(data);
+			
+			const promises = Array.from(document.querySelectorAll('.supertaste-coupon .coupon-list')).map(container => {
+				return new Promise(resolve => {
+					container.innerHTML = this.coupons.map(coupon => {
+						switch(coupon.type){
+							case 'HTML畫版':
+								if(coupon.image) {
+									coupon = `<div class="coupon-item splide__slide">
+										<div class="coupon-item-wrap">
+											<div class="coupon-item-img" style="background-image:url(${coupon.image})"></div>
+											<div class="coupon-item-content">
+												<div><a href="${coupon.link}" target="_blank">${coupon.title}</a></div>
+												<div>${coupon.title_info}</div>
+												<div><a href="${coupon.info_link}" target="_blank">查看店家資訊</a></div>
+											</div>
+										</div>
+									</div>`;
+								}else{
+									coupon = `<div class="coupon-item splide__slide">
+										<div class="coupon-item-wrap no-img">
+											<div class="coupon-item-content">
+												<div><a href="${coupon.link}" target="_blank">${coupon.title}</a></div>
+												<div>${coupon.title_info}</div>
+												<div><a href="${coupon.info_link}" target="_blank">查看店家資訊</a></div>
+											</div>
+										</div>
+									</div>`;
+								}
+								break;
+							case '全圖模式':
+								coupon = `<div class="coupon-item splide__slide">
+									<div class="coupon-item-wrap">
+										<div><a href="${coupon.link}" target="_blank"><img src="${coupon.image}" alt="${coupon.title}"></a></div>
+									</div>
+								</div>`;
+								break;
+						}
 
-					return coupon;
-				}).join('');
-				resolve();
+						return coupon;
+					}).join('');
+					resolve();
+				});
 			});
-		});
 
-		Promise.all(promises).then(() => {
-			if(!window.Splide) {
-				const script = document.createElement('script');
-				script.src = 'https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js';
-				document.body.appendChild(script);
+			Promise.all(promises).then(() => {
+				if(!window.Splide) {
+					const script = document.createElement('script');
+					script.src = 'https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js';
+					document.body.appendChild(script);
 
-				const style = document.createElement('link');
-				style.rel = 'stylesheet';
-				style.href = 'https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css';
-				document.head.appendChild(style);
+					const style = document.createElement('link');
+					style.rel = 'stylesheet';
+					style.href = 'https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css';
+					document.head.appendChild(style);
 
-				script.onload = async () => {
-					await new Promise(resolve => setTimeout(resolve, 1000)); // 等待1秒，确保脚本加载完成
+					script.onload = async () => {
+						await new Promise(resolve => setTimeout(resolve, 1000)); // 等待1秒，确保脚本加载完成
 
-					new Splide('.supertaste-coupon', {
-						type: 'loop',
-						autoplay: true,
-						interval: 3000,
-						pagination: false,
-						arrows: false,
-						perPage: 1,
-						height: 105,
-						gap: 16,
-					}).mount();
+						new Splide('.supertaste-coupon', {
+							type: 'loop',
+							autoplay: true,
+							interval: 3000,
+							pagination: false,
+							arrows: false,
+							perPage: 1,
+							height: 105,
+							gap: 16,
+						}).mount();
+					}
 				}
-			}
+			});
 		});
 	}
 };
