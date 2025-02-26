b28208d125f9c375f3665213a82197f9a407000e
linsly-achang <76680239+linsly-achang@users.noreply.github.com>
2025-02-26 15:46:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -92,8 +92,9 @@ const SuperCoupon = {
 		document.head.appendChild(link);
 	},
 	async loadCoupons(){
-		fetch('https://ads.achang.tw/super-coupon/').then(res => res.json()).then(data => {
-			console.log(data);
+		fetch('https://ads.achang.tw/super-coupon/index.php').then(res => res.json()).then(data => {
+			const fetchData = JSON.parse(data);
+			console.log(fetchData)
 		});
 		const promises = Array.from(document.querySelectorAll('.supertaste-coupon .coupon-list')).map(container => {
 			return new Promise(resolve => {
