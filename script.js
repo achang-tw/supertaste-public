b87cde92b02a028cc655db349d4ecd5e46b7be46
linsly-achang <76680239+linsly-achang@users.noreply.github.com>
2025-03-10 18:21:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -120,6 +120,7 @@ const SuperCoupon = {
 		});
 	},
 	async drawHTML(){
+		console.log(this.coupons)
 		if(this.coupons.length > 0){
 			Array.from(document.querySelectorAll('.supertaste-coupon .coupon-list')).map(container => {
 				container.innerHTML = this.pickCoupons(this.coupons, 1).map(coupon => {
