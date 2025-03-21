f36de46f7f6e8a9973915073e5d3b94b9b461b00
linsly-achang <76680239+linsly-achang@users.noreply.github.com>
2025-03-21 11:35:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -130,6 +130,13 @@ const SuperCoupon = {
 			return now >= start && now <= end;
 		});
 	},
+	stringToDate(dateString){
+		const [datePart, timePart] = dateString.split(' ');
+		const [year, month, day] = datePart.split('-').map(Number);
+		const [hours, minutes, seconds] = timePart.split(':').map(Number);
+
+		return new Date(year, month - 1, day, hours, minutes, seconds);
+	},
 	async drawHTML(){
 		const validCoupons = this.validCoupons(this.coupons);
 		if(validCoupons.length > 0){
